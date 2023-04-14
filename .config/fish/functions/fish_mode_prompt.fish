91ab23c782a5dd02b759f98a54929bb955c75639
Keenan Gugeler <me@kgugeler.ca>
2023-04-13 22:55:23 -0400

    Use black in mode prompt
    Honestly, both black and white look bad with catppuccin, but I like the
    theme, and black looks better and is easier to read.

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -4,19 +4,19 @@ function fish_mode_prompt
         or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
         switch $fish_bind_mode
             case default
-                set_color --bold --background red brwhite
+                set_color --bold --background red black
                 echo '[N]'
             case insert
-                set_color --bold --background green brwhite
+                set_color --bold --background green black
                 echo '[I]'
             case replace_one
-                set_color --bold --background green brwhite
+                set_color --bold --background green black
                 echo '[R]'
             case replace
-                set_color --bold --background cyan brwhite
+                set_color --bold --background cyan black
                 echo '[R]'
             case visual
-                set_color --bold --background magenta brwhite
+                set_color --bold --background magenta black
                 echo '[V]'
         end
         set_color normal
