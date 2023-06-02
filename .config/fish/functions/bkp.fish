fba3b59228f37fa0f0282959908f7de34e5c54d2
Keenan Gugeler <me@kgugeler.ca>
2023-06-02 11:21:11 -0400

    Move backup logic to restic script

--- .config/fish/functions/bkp.fish
+++ .config/fish/functions/bkp.fish
@@ -1,5 +1,3 @@
 function bkp --description "Perform a backup"
-    bw sync
-    and bw export --format encrypted_json --output ~/Backups/bitwarden_encrypted_export.json
-    and ~/.restic/backup.fish $argv
+    ~/.restic/backup.fish $argv
 end
